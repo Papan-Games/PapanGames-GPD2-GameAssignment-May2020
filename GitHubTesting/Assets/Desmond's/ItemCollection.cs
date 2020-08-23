3c1a6c524a321e6e8f0cc93f2cc89f2654a04e6a
DesmondYeoh-LeineX <desmondyeoh22@gmail.com>
2020-08-24 02:17:15 +0800

    Desmond's Fix Attempt

---
+++ GitHubTesting/Assets/Desmond's/ItemCollection.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollection : MonoBehaviour
+{
+    public OutlineController outlineRef;
+
+    // public bool Gun;
+    // public bool Keycard;
+    // public bool item3;
+    // public bool item4;
+
+    public enum Items {Gun, Keycard, Item3, Item4}
+    public Items item;
+    private void Start() 
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(outlineRef.showingOutline)
+        {
+            if(Input.GetKey(KeyCode.E))
+            {
+                switch(item)
+                {
+                    case Items.Gun:
+                    {
+                        InventoryManager.instance.GetGun();
+                        this.gameObject.SetActive(false);
+                        Debug.Log("Store Gun");
+                        break;
+                    }
+                    case Items.Keycard:
+                    {
+                        InventoryManager.instance.GetKeycard();
+                        Debug.Log("Store Card");
+                        break;
+                    }
+                    case Items.Item3:
+                    {
+                        Debug.Log("Store Shit1");
+                        break;
+                    }
+                    case Items.Item4:
+                    {
+                        Debug.Log("Store Shit2");
+                        break;
+                    }
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ GitHubTesting/Assets/Desmond's/OutlineController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineController : MonoBehaviour
+{
+    private MeshRenderer _renderer;
+    public bool showingOutline;
+    public float maxOutlineWidth;
+    public Color OutlineColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        showingOutline = false;
+        _renderer = gameObject.GetComponent<MeshRenderer>();
+    }
+
+    public void ShowOutline()
+    {
+        showingOutline = true;
+        foreach (var mat in _renderer.materials)
+        {
+            mat.SetFloat("_Outline", maxOutlineWidth);
+            mat.SetColor("_OutlineColor", OutlineColor);
+        }
+    }
+
+    public void HideOutline()
+    {
+        showingOutline = false;
+        foreach (var mat in _renderer.materials)
+        {
+            mat.SetFloat("_Outline", 0f);
+            mat.SetColor("_OutlineColor", Color.black);
+        }
+    }
+}
