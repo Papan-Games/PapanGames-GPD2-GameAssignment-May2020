d12aa14bde1487866f1c47e10a52c3001efe307d
peiyi456 <36634171+peiyi456@users.noreply.github.com>
2020-08-25 02:59:24 +0800

    update menu Scene

---
+++ GitHubTesting/Assets/Peiyi/Scripts/MenuOptions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuOptions : MonoBehaviour
+{
+    //public GameObject controlIntructionPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //controlIntructionPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void onPlayButton()
+    {
+        SceneManager.LoadScene("FirstLevel_Peiyi");
+    }
+
+    //public void onCtlIntrucBttn()
+    //{
+    //    controlIntructionPanel.SetActive(true);
+    //}
+
+    public void onQuitButton()
+    {
+        Application.Quit();
+    }
+}
--- GitHubTesting/Assets/Peiyi/Scripts/StartingDialogue.cs
+++ GitHubTesting/Assets/Peiyi/Scripts/StartingDialogue.cs
@@ -35,4 +35,6 @@ public class StartingDialogue : MonoBehaviour
 
         
     }
+
+    
 }
